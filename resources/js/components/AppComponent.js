2de7b1301f728d45216b5350018572e785a5de01
Qaisar Abbas <qaisarabbas912@gmail.com>
2020-01-16 23:41:19 +0400

    First Commit Django and React JS

---
+++ resources/js/components/AppComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Dashboard from './Dashboard';
+
+export default class AppComponent extends Component {
+    render() {
+        return (
+            <div>
+                < Dashboard />
+            </div>
+        )
+    }
+}
---
+++ webpack.mix.js
@@ -0,0 +1,2 @@
+const mix = require('laravel-mix');
+mix.react('resources/js/app.js', './static/js').sass('./resources/sass/app.scss', './static/css');
